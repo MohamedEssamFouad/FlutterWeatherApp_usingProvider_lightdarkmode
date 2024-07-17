930af1b041a9677eb09fd20af1824a1e8e4847b0
MohamedAltay <145346246+MohamedEssamFouad@users.noreply.github.com>
2024-07-17 04:37:29 +0300

    make it adaptive

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -17,27 +17,25 @@ class Home extends StatelessWidget {
         ),
         actions: [
           Padding(
-            padding:  EdgeInsets.only(right: 15.w),
+            padding: EdgeInsets.only(right: 15.w),
             child: Consumer<PostProvider>(
-              builder: (context, themeProider, child) {
+              builder: (context, themeProvider, child) {
                 return Switch(
                   activeColor: Colors.white,
-                  // trackOutlineColor:
-                  //     const MaterialStatePropertyAll(Colors.black),
                   inactiveThumbColor: Colors.white,
                   thumbColor: const MaterialStatePropertyAll(Colors.orange),
                   inactiveTrackColor: Colors.transparent,
                   thumbIcon: MaterialStatePropertyAll(
-                    themeProider.isSwitched
+                    themeProvider.isSwitched
                         ? const Icon(Icons.nights_stay)
                         : const Icon(
                       Icons.wb_sunny,
                       color: Colors.white,
                     ),
                   ),
-                  value: themeProider.isSwitched,
+                  value: themeProvider.isSwitched,
                   onChanged: (value) {
-                    themeProider.toggleTheme();
+                    themeProvider.toggleTheme();
                   },
                 );
               },
@@ -45,86 +43,93 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25,vertical: 25.0),
-        child: Center(
-          child: Column(
-            children: [
-              Text(
-                'Paris',
-                style: TextStyle(fontSize:40.sp ),
-              ),
-              SizedBox(height: 10.h,),
-              Consumer<PostProvider>(
-                builder: (context,themeprovider,child){
-                  return  Icon(
-                  themeprovider.isSwitched?
-
-                    Icons.nights_stay:Icons.sunny,size: 300,color: Colors.orange,
-                  );
-                },
-
-              ),
-              SizedBox(height: 40.h,),
-              Text(
-                '20 c',style: TextStyle(fontSize: 20.sp,fontWeight: FontWeight.bold),
-
-              ),
-              SizedBox(height: 20.h,),
-              Text(
-                'Good Morning',
-                style: TextStyle(
-                  fontSize: 15.sp,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  'Paris',
+                  style: TextStyle(fontSize: 40.sp),
                 ),
-              ),SizedBox(height: 30.h,),
-              SizedBox(
-                width: 50,
-                child: Divider(
-                  thickness: 3,
+                SizedBox(height: 10.h),
+                Consumer<PostProvider>(
+                  builder: (context, themeProvider, child) {
+                    return Icon(
+                      themeProvider.isSwitched
+                          ? Icons.nights_stay
+                          : Icons.wb_sunny,
+                      size: 300,
+                      color: Colors.orange,
+                    );
+                  },
                 ),
-              ),
-             Spacer(),
-               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Column(
-                    children: [
-                      Icon(Icons.wb_twighlight),
-                      Text('SUNRISE'),
-                      Text('7:00'),
-                    ],
+                SizedBox(height: 40.h),
+                Text(
+                  '20 c',
+                  style: TextStyle(
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.bold,
                   ),
-                  SizedBox(
-                    height: 50.h,
-                    child: VerticalDivider(
-                      color: Colors.black,
-                      thickness: 1,
+                ),
+                SizedBox(height: 20.h),
+                Text(
+                  'Good Morning',
+                  style: TextStyle(
+                    fontSize: 15.sp,
+                  ),
+                ),
+                SizedBox(height: 30.h),
+                SizedBox(
+                  width: 50,
+                  child: Divider(
+                    thickness: 3,
+                  ),
+                ),
+                SizedBox(height: 20.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Column(
+                      children: [
+                        Icon(Icons.wb_twighlight),
+                        Text('SUNRISE'),
+                        Text('7:00'),
+                      ],
                     ),
-                  ),
-                  const Column(
-                    children: [
-                      Icon(Icons.air),
-                      Text('Wind'),
-                      Text('4m/s'),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 50.h,
-                    child: VerticalDivider(
-                      color: Colors.black,
-                      thickness: 1,
+                    SizedBox(
+                      height: 50.h,
+                      child: VerticalDivider(
+                        color: Colors.black,
+                        thickness: 1,
+                      ),
                     ),
-                  ),
-                  const Column(
-                    children: [
-                      Icon(Icons.thermostat),
-                      Text('Temperature'),
-                      Text('23'),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                    const Column(
+                      children: [
+                        Icon(Icons.air),
+                        Text('Wind'),
+                        Text('4m/s'),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 50.h,
+                      child: VerticalDivider(
+                        color: Colors.black,
+                        thickness: 1,
+                      ),
+                    ),
+                    const Column(
+                      children: [
+                        Icon(Icons.thermostat),
+                        Text('Temperature'),
+                        Text('23'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
